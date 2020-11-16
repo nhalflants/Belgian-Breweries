a1781768900bd657c1aa101dcdb39bc518917280
nhalflants <natacha_halflants@hotmail.com>
2020-11-16 12:29:41 +0100

    Adding Logging

--- Breweries/Pages/Breweries/List.cshtml.cs
+++ Breweries/Pages/Breweries/List.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Breweries.Data;
 using Breweries.Core;
+using Microsoft.Extensions.Logging;
 
 namespace Breweries.Pages.Breweries
 {
@@ -18,14 +19,19 @@ namespace Breweries.Pages.Breweries
         public string SearchTerm { get; set; }
         public readonly IConfiguration configuration;
         private readonly IBreweriesService breweries;
+        private readonly ILogger<ListModel> logger;
 
-        public ListModel(IConfiguration configuration, IBreweriesService breweries)
+        public ListModel(IConfiguration configuration, 
+            IBreweriesService breweries,
+            ILogger<ListModel> logger)
         {
             this.configuration = configuration;
             this.breweries = breweries;
+            this.logger = logger;
         }
         public void OnGet()
         {
+            logger.LogError("Executing ListModel");
             Message = configuration["Message"];
             Breweries = breweries.GetBreweriesByName(SearchTerm);
         }
